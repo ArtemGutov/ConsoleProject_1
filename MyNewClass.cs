6c94e41e6dd043f79911ce171ddfa952f997a742
ArtemGutov <artem.gutov@yandex.ru>
2025-11-27 14:41:27 +0300

    Новая ветка

---
+++ MyNewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject_1
+{
+    public class MyNewClass
+    {
+        public MyNewClass()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
